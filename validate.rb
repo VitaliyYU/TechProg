da1a327cbdf3db58e1ff176816c37882b2365713
Vitakil <yurchuk2@mail.ru>
2017-11-27 10:02:52 +0200

    reset branches

--- validate.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'nokogiri'
-
-xsd = Nokogiri::XML::Schema(File.read('Dance.xsd'))
-doc = Nokogiri::XML(File.read('Dance.xml'))
-
-xsd.validate(doc).each do |error|
-  puts error.message
-end
